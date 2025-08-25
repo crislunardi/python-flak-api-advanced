acc48a1b1172728cb9b149a1514d95e1d9353f21
Cristian Lunardi <c.lunarditi@gmail.com>
2025-08-25 20:49:00 -0300

    feat: add logout route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -15,7 +15,7 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id))
+    return User.query.get(user_id)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -33,6 +33,12 @@ def login():
       return jsonify({"message": "Autenticação realizada com sucesso!"})
 
   return jsonify({"message": "Credenciais inválidas"}), 400
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
